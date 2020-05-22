046cf2837b5e8d732f7685401355c1f8a359e258
chxbriyani <65714405+chxbriyani@users.noreply.github.com>
2020-05-22 17:13:14 +0530

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,15 @@
+import random
+
+hidden = random.randrange(1,100)
+i=0
+print(hidden)
+while i<10:
+ guess=int(input("input number"))
+ if (guess==hidden):
+  print("right")
+  break
+ elif(guess<hidden):
+  print("low")
+ else:
+  print("high")
+ i+=1
